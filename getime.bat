9930163a796429bb60e3e9d2cea2be9611f65230
AutHor <adelaumnonegligas@gmail.com>
2021-07-19 17:23:45 -0500

    1.-Obtiene Ti 2.-usa timeout 3.-obtiene Tf 4.-extrae unidades de ambos 5.-muestra los tiempos

---
+++ getime.bat
@@ -0,0 +1,80 @@
+@ECHO OFF
+
+
+REM ---------------------------ACERCA DE ESTE PROGRAMA-------------------------------
+REM |        NOMBRE: timer.bat                                                      |
+REM |                                                                               |
+REM |        VERSION DEL PROGRAMA : V 1.0                                           |
+REM |                                                                               |
+REM |        DESCRIPCION: EL PROGRAMA MIDE EL TIEMPO DE EJECUCION DE OTROS          |
+REM |                     PROGRAMAS BASÁNDOSE EN EL VALOR DE LA PSEUDO VARIABLE     |
+REM |                     %TIME% Y PINTA LA DURACION POR PANTALLA                   |
+REM |                                                                               |
+REM |        AUTOR: HELIOS BARRERA HERNÁNDEZ                                        |
+REM |        CONTACTO: adealumnonegligas@gmail.com                                  |
+REM |                                                                               |
+REM |        NOTAS: 1.-ESTE PROGRAMA UTILIZA EL ESTÁNDAR "A1" EL CUAL DEFINE LO     |
+REM |                  SIGUIENTE: 1. LA FORMA EN LA QUE SE NOMBRAN : LAS VARIABLES  |
+REM |                                Y LAS FUNCIONES Y EL PROGRAMA                  |
+REM |                             2. ESTRUCTURA DEL PROGRAMA                        |
+REM |                             3. VALIDACION DE ARGUMENTOS                       |
+REM |                             4. INSTRUCTIVO DE USO DEL PROGRMA                 |
+REM |                                (ESTRUCTURA DE LA FUNCION "USAGE")             |
+REM |                             5. FORMA DE DOCUMENTACION INTERNA Y EXTERNA       |
+REM |                             6. ESTRUCTURA DEL DIAGRAMA DE FLUJO               |
+REM |                                                                               |
+REM |               2.-EL PROGRAMA USA SECUENCIAS DE ESCAPE ANSI PARA COLOREAR LOS  |
+REM |                  MENSAJES RELEVANTES POR LO QUE REQUIERE QUE EL SO SEA        |
+REM |                  WINDOWS 10 COMPILACION MAYOR A 1909                          |
+REM |                                                                               |
+REM ---------------------------------------------------------------------------------
+
+
+TITLE GETIME 
+
+
+REM PROCESO_almacenar Tiempo inicial-> pintarlo,PAUSAR, almacenar Tiempo final -> pintarlo 
+SET _tInicial=%TIME%
+ECHO tiempo inicial %_tInicial%
+TIMEOUT /T -1 >NUL
+SET _tFinal=%TIME%
+ECHO tiempo final %_tFinal%
+
+REM PROCESO_obtener unidades por separado y almacenando como valores numéricos 
+::tiempo inicial luego tiempo final (3 y 3 ) 
+::INICIALES
+SET /A _centiSegInicial=%_tInicial:~9,2%
+SET /A _segInicial=%_tInicial:~6,2%
+SET /A _minInicial=%_tInicial:~3,2%
+SET /A _horaInicial=%_tInicial:~0,2%
+
+REM DEBUG_tiempo inicial
+ECHO fulltime inicial %_tInicial%
+ECHO sub %_horaInicial%:%_minInicial%:%_segInicial%.%_centiSegInicial%
+
+::FINALES
+SET /A _centiSegFinal=%_tFinal:~9,2%
+SET /A _segFinal=%_tFinal:~6,2%
+SET /A _minFinal=%_tFinal:~3,2%
+SET /A _horaFinal=%_tFinal:~0,2%
+
+REM DEBUG_tiempo final
+ECHO FULLTIME FINAL %_tFinal%
+ECHO sub %_horaFinal%:%_minFinal%:%_segFinal%.%_centiSegFinal%
+
+
+REM CONCICIONAL_unidadesTi>unidadesTf -> ALGORITMO DE CONVERSION de lo contrario APLICAR FORMULA
+
+	
+
+REM PROCESO_ALGORITMO DE CONVERSION
+
+
+
+:EOF 
+EXIT /B 0
+
+
+REM REVISA LA DOCUMENTACION DE 
+:: SET /A /? EL SIGUIENTE TEXTO ES MOSTRADO 
+:: 08 y 09 no son números válidos porque 8 y 9 no son dígitos octales válidos.
